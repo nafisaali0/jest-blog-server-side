657da042143406c581f41f69370ef21e5e94e093
nafisaali0 <nafisaali20006@gmail.com>
2023-11-20 18:57:55 +0600

    add final

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const blogCollection = client.db("blogWeb").collection("blogs");
     const categoryCollection = client.db("blogWeb").collection("category");
